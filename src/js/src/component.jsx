adf608aab5a0e813431d635f74abae28d0d10c07
aik <aik>
2017-08-11 16:10:25 +0100

    commited

--- src/js/src/component.jsx
+++ src/js/src/component.jsx
@@ -14,7 +14,7 @@ export class SHOPPING_LIST extends React.Component
     render()
     {
         return <div>
-                <h1>title: {this.props.vars.title}</h1>
+                <h2>title: {this.props.vars.title}</h2>
                 <SHOPPING_LIST_ITEM name={this.props.vars.item0} />
             </div>;
     }
