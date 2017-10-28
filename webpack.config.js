0fa67410f170e8698a3bf586683462a1c8eac69a
aik6980 <aik6980>
2017-10-28 08:17:28 +0100

    wip gulp->webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+const PATH = require('path');
+const CLEAN_WEBPACK_PLUGIN = require('clean-webpack-plugin');
+
 module.exports = {
     entry: __dirname + '/src/js/src/game.js',
+    plugins: [
+        new CLEAN_WEBPACK_PLUGIN(['public']) 
+    ],
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
+                include: PATH.resolve(__dirname, "src"),
                 loader: 'babel-loader'
             }
         ]
